ff4218f35bf34f69fbcda4795d398dc84ecf39d8
jagrecco <jagrecco@gmail.com>
2022-12-11 21:10:39 -0300

    cambio en el homepage del proyecto

--- api/models/Usuarios.js
+++ api/models/Usuarios.js
@@ -1,5 +1,5 @@
 /**
- * Productos.js
+ * Usuarios.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -8,12 +8,12 @@
 module.exports = {
 
   attributes: {
+
     id: { type: 'string', columnName: '_id' },
     name: { type: 'string', required: true },
-    description: { type: 'string', required: true },
-    price: { type: 'number', required: true },
-  
+    password: { type: 'string', required: true },
+
   },
-  
+
 };
 
